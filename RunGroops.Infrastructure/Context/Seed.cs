1dd217ed6e7115987d05121e246e06e1f3ef120a
TheStankela <ognjenogistankovic2002@gmail.com>
2023-07-02 18:25:09 +0200

    Added new authentication and identity framework

---
+++ RunGroops.Infrastructure/Context/Seed.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using RunGroops.Domain.EFModels;
+using RunGroops.Domain.Enum;
+
+namespace RunGroops.Infrastructure.Context
+{
+    public class Seed
+    {
+        public static async Task SeedUsersAndRolesAsync(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                //Roles
+                var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                if (!await roleManager.RoleExistsAsync(UserRoles.Admin))
+                    await roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+                if (!await roleManager.RoleExistsAsync(UserRoles.User))
+                    await roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+
+                //Users
+                var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+                string adminUserEmail = "admin@gmail.com";
+
+                var adminUser = await userManager.FindByEmailAsync(adminUserEmail);
+                if (adminUser == null)
+                {
+                    var newAdminUser = new AppUser()
+                    {
+                        UserName = "AdminUserName",
+                        Email = adminUserEmail,
+                        EmailConfirmed = true,
+                        Address = new Address()
+                        {
+                            Street = "Ulica 123",
+                            City = "Zrenjanin",
+                            Country = "Serbia",
+                            Zip = 23000
+                        }
+                    };
+                    await userManager.CreateAsync(newAdminUser, "Admin123!");
+                    await userManager.AddToRoleAsync(newAdminUser, UserRoles.Admin);
+                }
+
+                string appUserEmail = "user@gmail.com";
+
+                var appUser = await userManager.FindByEmailAsync(appUserEmail);
+                if (appUser == null)
+                {
+                    var newAppUser = new AppUser()
+                    {
+                        UserName = "UserUserName",
+                        Email = appUserEmail,
+                        EmailConfirmed = true,
+                        Address = new Address()
+                        {
+                            Street = "Ulica 456",
+                            City = "Zrenjanin",
+                            Country = "Serbia",
+                            Zip = 23000
+                        }
+                    };
+                    await userManager.CreateAsync(newAppUser, "User123!");
+                    await userManager.AddToRoleAsync(newAppUser, UserRoles.User);
+                }
+            }
+        }
+    }
+}
