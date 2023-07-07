f6e2020e7150128c7499daee96f76aa4d94cab03
TheStankela <ognjenogistankovic2002@gmail.com>
2023-07-07 18:22:24 +0200

    Changed model

--- RunGroops.Application/Models/UserRegisterRequest.cs
+++ RunGroops.Application/Models/UserRegisterRequest.cs
@@ -1,9 +1,15 @@
-﻿namespace RunGroops.Application.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroops.Application.Models
 {
     public class UserRegisterRequest
     {
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
         public string ConfirmPassword { get; set; }
 
     }
---
+++ RunGroops.Application/Models/Value.cs
@@ -0,0 +1,9 @@
+﻿namespace RunGroops.Application.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
