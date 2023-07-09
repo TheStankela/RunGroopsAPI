b83619d74ab6e9c2fb816d9ac0b2d620769ef846
TheStankela <ognjenogistankovic2002@gmail.com>
2023-07-09 23:17:21 +0200

    Added picture upload

--- RunGroops.Application/Handlers/ClubHandlers/AddClubHandler.cs
+++ RunGroops.Application/Handlers/ClubHandlers/AddClubHandler.cs
@@ -1,8 +1,11 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using RunGroops.Application.Commands.ClubCommands;
 using RunGroops.Application.Helpers;
 using RunGroops.Application.Services;
 using RunGroops.Domain.Interfaces;
+using System.Security.Claims;
 
 namespace RunGroops.Application.Handlers.ClubHanders
 {
@@ -10,19 +13,28 @@ namespace RunGroops.Application.Handlers.ClubHanders
     {
         private readonly IClubRepository _clubRepository;
         private readonly IClubMapper _mapper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IPhotoService _photoService;
 
-        public AddClubHandler(IClubRepository clubRepository, IClubMapper mapper, IPhotoService photoService)
+        public AddClubHandler(IClubRepository clubRepository, IClubMapper mapper, IHttpContextAccessor httpContextAccessor, IPhotoService photoService)
         {
             _clubRepository = clubRepository;
             _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
             _photoService = photoService;
         }
 
         public async Task<bool> Handle(AddClubCommand request, CancellationToken cancellationToken)
         {
+            var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            if (userId is null)
+                return false;
+            
+            var clubMapped = _mapper.MapClubRequestToClub(request.ClubRequest, userId);
+            
+            var uploadResult = _photoService.AddPhotoAsync(request.file);
 
-            var clubMapped = _mapper.MapClubRequestToClub(request.ClubRequest);
+            clubMapped.ImageURL = uploadResult.Result.Url.ToString();
 
             return await _clubRepository.AddClubAsync(clubMapped);
         }
