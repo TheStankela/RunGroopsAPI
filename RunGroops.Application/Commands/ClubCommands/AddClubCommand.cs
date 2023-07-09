a0f9252c6eaae027a407f49ec880305a8c360c97
TheStankela <ognjenogistankovic2002@gmail.com>
2023-07-09 23:17:09 +0200

    Added picture upload

--- RunGroops.Application/Commands/ClubCommands/AddClubCommand.cs
+++ RunGroops.Application/Commands/ClubCommands/AddClubCommand.cs
@@ -5,5 +5,6 @@ using RunGroops.Application.Models;
 namespace RunGroops.Application.Commands.ClubCommands
 {
     public record AddClubCommand(
-        ClubRequest ClubRequest) : IRequest<bool>;
+        ClubRequest ClubRequest,
+        IFormFile file) : IRequest<bool>;
 }
