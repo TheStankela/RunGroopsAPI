62d0a6545179777944f0b076c144312c47ad310e
TheStankela <ognjenogistankovic2002@gmail.com>
2023-07-19 01:33:50 +0200

    Added race controller and all endpoinds needed

--- RunGroopsAPI/Controllers/RaceController.cs
+++ RunGroopsAPI/Controllers/RaceController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using RunGroops.Application.Commands.RaceCommands;
+using RunGroops.Application.Models;
+using RunGroops.Application.Queries.RaceQueries;
 using RunGroops.Domain.Interfaces;
 
 namespace RunGroopsAPI.Controllers
@@ -8,47 +12,73 @@ namespace RunGroopsAPI.Controllers
     [ApiController]
     public class RaceController : ControllerBase
     {
-        private readonly IRaceRepository _raceRepository;
-        public RaceController(IRaceRepository raceRepository)
+        private readonly IMediator _mediator;
+        public RaceController(IRaceRepository raceRepository, IMediator mediator)
         {
-            _raceRepository = raceRepository;
+            _mediator = mediator;
         }
         [HttpGet]
         public async Task<IActionResult> GetRacesAsync([FromQuery]int page)
         {
-            return Ok();
+            var query = new GetRacesQuery(page);
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
         }
         [Authorize]
         [HttpGet("user")]
         public async Task<IActionResult> GetAllUserRacesAsync()
         {
-            return Ok();
+            var query = new GetAllUserRacesQuery();
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetRaceByIdAsync(int id)
         {
-            return Ok();
+            var query = new GetRaceByIdQuery(id);
+            var result = await _mediator.Send(query);
+
+            return result is not null ? Ok(result) : NotFound();
         }
-        [HttpGet("city={cityName}")]
+        [HttpGet("name={raceName}")]
+        public async Task<IActionResult> GetRacesByNameAsync(string raceName)
+        {
+            var query = new GetRacesByNameQuery(raceName);
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+        [HttpGet("city/{cityName}")]
         public async Task<IActionResult> GetRacesByCityAsync(string cityName)
         {
-            return Ok();
+            var query = new GetRacesByCityQuery(cityName);
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
         }
         [Authorize]
         [HttpPost]
-        public async Task<IActionResult> AddRaceAsync()
+        public async Task<IActionResult> AddRaceAsync([FromForm]RaceRequest request, IFormFile file)
         {
-            return Ok();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var command = new AddRaceCommand(request, file);
+            var result = await _mediator.Send(command);
+
+            return result is true ? Ok(new { Message = "Created Successfully!"}) : BadRequest(ModelState);
         }
         [Authorize]
         [HttpPut]
-        public async Task<IActionResult> UpdateRaceAsync([FromQuery]int raceId)
+        public async Task<IActionResult> UpdateRaceAsync([FromQuery]int raceId, RaceRequest raceRequest)
         {
             return Ok();
         }
         [Authorize]
         [HttpDelete]
-        public async Task<IActionResult> DeleteRaceAsync([FromQuery] int clubId)
+        public async Task<IActionResult> DeleteRaceAsync([FromQuery] int raceId)
         {
             return Ok();
         }
