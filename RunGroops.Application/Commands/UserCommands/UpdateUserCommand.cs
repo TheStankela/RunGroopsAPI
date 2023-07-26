49cd48241558ee479190dfee2be445228caff58f
TheStankela <ognjenogistankovic2002@gmail.com>
2023-07-27 00:02:08 +0200

    Update user command implemented

---
+++ RunGroops.Application/Commands/UserCommands/UpdateUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using RunGroops.Application.Models;
+
+namespace RunGroops.Application.Commands.UserCommands
+{
+    public record UpdateUserCommand(
+        string UserId,
+        UpdateUserRequest Request) : IRequest<bool>;
+}
