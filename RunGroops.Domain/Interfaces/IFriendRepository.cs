5e0e4e94a66cc00cfff31ee0acbf58c84449eb16
TheStankela <ognjenogistankovic2002@gmail.com>
2023-07-27 00:06:06 +0200

    Created repository interface with all methods

---
+++ RunGroops.Domain/Interfaces/IFriendRepository.cs
@@ -0,0 +1,15 @@
+﻿using RunGroops.Domain.EFModels;
+
+namespace RunGroops.Domain.Interfaces
+{
+    public interface IFriendRepository
+    {
+        Task<int> GetUserFriendsCount(string userId);
+        Task<ICollection<Friend>> GetUserPendingFriendRequests(string userId);
+        Task<Friend?> GetRequestByUserIds(string fromUserId, string toUserId);
+        Task<bool> UpdateFriendRequest(Friend friend);
+        Task<bool> FriendRequestExists(string fromUserId, string toUserId);
+        Task<bool> CreateFriendRequest(Friend friend);
+
+    }
+}
