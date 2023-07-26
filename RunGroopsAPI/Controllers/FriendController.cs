ae141b4e8f4ae3e3f6e535b2a777e5fe8678aea0
TheStankela <ognjenogistankovic2002@gmail.com>
2023-07-27 00:07:35 +0200

    Created controller and added get and post methods

---
+++ RunGroopsAPI/Controllers/FriendController.cs
@@ -0,0 +1,57 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RunGroops.Application.Commands.FriendCommands;
+using RunGroops.Application.Queries.FriendQueries;
+using RunGroops.Domain.EFModels;
+using RunGroops.Domain.Enum;
+
+namespace RunGroopsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FriendController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public FriendController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("Count")]
+        public async Task<IActionResult> GetUserFriendsCount([FromQuery]string userId)
+        {
+            var query = new GetUserFriendsCountQuery(userId);
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+        [Authorize]
+        [HttpGet("Requests")]
+        public async Task<IActionResult> GetUserPendingFriendRequests()
+        {
+            var query = new GetUserPendingFriendRequestsQuery();
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+        [Authorize]
+        [HttpPost("Request/Accept")]
+        public async Task<IActionResult> AcceptFriendRequest([FromQuery] string fromUserId)
+        {
+            var command = new AcceptFriendRequestCommand(fromUserId);
+            var result = await _mediator.Send(command);
+
+            return result is true ? Ok(new {Message = "Accepted successfully!"}) : BadRequest(ModelState);
+        }
+        [Authorize]
+        [HttpPost("Request/Send")]
+        public async Task<IActionResult> SendFriendRequest([FromQuery]string toUserId)
+        {
+            var command = new SendFriendRequestCommand(toUserId);
+            var result = await _mediator.Send(command);
+
+            return result is true ? Ok(new { Message = "Sent successfully!" }) : BadRequest(ModelState);
+        }
+    }
+}
